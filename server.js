0d8ce105b2723074890fb480b4e6341ee38b9ee7
Myllena <58570477+myygarcia@users.noreply.github.com>
2021-08-25 22:33:33 -0300

    heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"))
+
+const http = require('http').Server(app)
+
+
+const porta = process.env.PORT || 3000
+
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
+
+http.listen(porta, function() {
+    const portaStr = porta === 80 ? '' : ':' + porta
+
+    if (process.env.HEROKU_APP_NAME)
+        console.log('Servidor iniciado. Abra o navegador em ' + host)
+    else
+        console.log('Servidor iniciado. Abra o naegador em ' + host + portaStr)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
